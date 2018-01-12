33224e6e6a8b79c8caeb04eea9d683016618fdf1
Marcos Paulo da Silva <mpsacademico@gmail.com>
2018-01-12 09:09:26 -0200

    Adição de padrão para criação de dados PTU

--- fks.py
+++ fks.py
@@ -1,8 +1,18 @@
 #! /usr/bin/python
 
-import sys
+import sys, random, time
 
-if len(sys.argv) > 1 and sys.argv[1]=="--live" :
-	print("--live")
+if len(sys.argv) > 1 and sys.argv[1]=="run" :	
+	file = open("/home2/marcos/pacova/ptu.txt","a") 
+	while 1:
+		ctime = time.strftime('%Y-%m-%d %H:%M:%S')
+		p = random.uniform(700, 900)
+		rh = random.uniform(50, 99)
+		t = random.uniform(10, 40)
+		line = "%s P= %.2fhPa  RH=  %.2f%%RH  T=  %.2f'C \r"%(ctime, p, rh, t)
+		file.write(line)
+		print(line)
+		time.sleep(1)
+	file.close()
 else:
 	print("Opcao nao disponivel")
